a9aee5f585c4e0aebabc8a6215c3d671f0df8438
mc-greq <greq19@gmail.com>
2019-02-12 21:29:32 +0100

    Set up persistence object entities

---
+++ src/example/nio/entities/Review.java
@@ -0,0 +1,45 @@
+package example.nio.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    public Review(){
+
+    }
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
